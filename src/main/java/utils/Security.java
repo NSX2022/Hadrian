42e967bc634aa81ba41e07c570fef2a949b32cfe
NSX2022 <nedstarkxy@gmail.com>
2026-02-04 10:53:43 -0500

    More explanation

--- src/main/java/utils/Security.java
+++ src/main/java/utils/Security.java
@@ -7,6 +7,9 @@ import java.nio.file.Path;
 
 public class Security {
     //TODO Security checks (e.g, checksum check on the .jar file using jar signing)
+    //We can implement this whenever, preferably right before release as jar signing complicates the build process
+    //For other security features, we can obfuscate the jar file, ban commonly used ports, etc.
+
     public Security(){
         File thisJar = new File(Security.class.getProtectionDomain().getCodeSource().getLocation().getPath());
         try {
