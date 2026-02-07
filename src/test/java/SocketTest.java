f949c03ccc8b640859394b2c2f5b486163e7dd8b
NSX2022 <nedstarkxy@gmail.com>
2026-02-07 16:09:57 -0500

    Sender framework, new SocketTest

--- src/test/java/SocketTest.java
+++ src/test/java/SocketTest.java
@@ -2,90 +2,38 @@
 // Posted by user3841581
 // Retrieved 2026-02-05, License - CC BY-SA 3.0
 
+import config.Config;
+import networking.Receiver;
+import networking.Sender;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.*;
 
 public class SocketTest {
-    private boolean run = true;
+    private static Config conf;
+    public static void Main(String[] args){
+        conf = new Config();
+        try {
+            conf.read_config();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
+        Receiver receiver = new Receiver(conf);
+        Sender sender = new Sender(conf);
 
-    public static void main(String[] args) throws IOException {
+        receiver.startServer();
+        receiver.setOpen(true);
 
-        startServer();
-        startSender();
-    }
+        InetAddress toSend;
+        try {
+            //Replace with my IP address
+            toSend = InetAddress.getByName("172.27.244.157");
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
 
-    public static void startSender() throws UnknownHostException{
-        InetAddress aHost = InetAddress.getLocalHost();
-        (new Thread() {
-            @Override
-            public void run() {
-                byte data[] = "Hello".getBytes();
-                DatagramSocket socket = null;
-                try {
-                    socket = new DatagramSocket();
-                    socket.setBroadcast(true);
-                } catch (SocketException ex) {
-                    ex.printStackTrace();
-                    //parent.quit();
-                }
-
-                DatagramPacket packet = new DatagramPacket(
-                        data,
-                        data.length,
-                        aHost,
-                        9090);
-
-                int i=0;
-                while (i<10) {
-                    try {
-                        System.out.println("SENDER packet data: "+new String(packet.getData()));
-                        socket.send(packet);
-                        Thread.sleep(50);
-                        i++;
-                        System.out.println(i);
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                        // parent.quit();
-                    } catch (InterruptedException ex) {
-                        ex.printStackTrace();
-                        // parent.quit();
-                    }
-                }
-            }}).start();
-    }
-
-
-    public static void startServer() {
-        (new Thread(() -> {
-
-            //byte data[] = new byte[0];
-            DatagramSocket socket = null;
-            try {
-                socket = new DatagramSocket(9090);
-                //socket.setBroadcast(true);;
-
-            } catch (SocketException ex) {
-                ex.printStackTrace();
-                //parent.quit();
-            }
-            DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
-            //System.out.println("this is what has been received111"+packet.getData());
-            String temp;
-            while (true) {
-                try {
-                    socket.receive(packet);
-                    temp=new String(packet.getData());
-                    System.out.println("RECEIVER: Raw packet data:: "+temp);
-
-
-                    //System.out.println("Message received ..."+ temp);
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                    //parent.quit();
-                }
-
-            }
-        })).start();
+        sender.sendMessage("SAVE THE CHICKENS", toSend);
     }
 }
