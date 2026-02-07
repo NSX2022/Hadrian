0420309daee3239e9051c950c4bbeff11ca7f443
calciumking <lkingerslev@gmail.com>
2026-02-07 17:39:07 -0500

    Created Chat and User dataclasses
    - Dataclasses hold as much information as I am currently aware they need
    
    - These will likely be improved as we progress
    
    - These dataclasses were made with the idea that a server would hold them, giving the user access to only the chats they are in

---
+++ src/main/java/models/Chat.java
@@ -0,0 +1,57 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Chat {
+    private final int localID;
+    private final HashSet<User> users;
+    private final LinkedList<String> messages;
+    
+    public Chat(int localID, ArrayList<User> members, String firstMessage) {
+        this.localID = localID;
+        users = new HashSet<>(members);
+        messages = new LinkedList<>();
+        
+        messages.add(firstMessage);
+    }
+    
+    public int getId() {
+        return localID;
+    }
+    
+    public HashSet<User> getUsers() {
+        return users;
+    }
+    
+    public LinkedList<String> getMessages() {
+        return messages;
+    }
+    
+    /**
+     * Adds a user to a chat's user collection.
+     *
+     * @param user User object to be added to collection
+     * @return true if user was not already in collection and added successfully, false otherwise.
+     * @see User
+     */
+    public boolean addUser(User user) {
+        return users.add(user);
+    }
+    
+    /**
+     * Removes a user from a chat's user collection
+     *
+     * @param user User object to be removed from collection
+     * @return true if user was in collection and removed successfully, false otherwise.
+     * @see User
+     */
+    public boolean removeUser(User user) {
+        return users.remove(user);
+    }
+    
+    public void addMessage(String message) {
+        messages.add(message);
+    }
+}
---
+++ src/main/java/models/User.java
@@ -0,0 +1,44 @@
+package models;
+
+import java.util.LinkedHashSet;
+
+public class User {
+    private String ip, username;
+    private final LinkedHashSet<Chat> chats;
+    
+    public User(String ip, String username) {  // new user
+        this.ip = ip;
+        this.username = username;
+        chats = new LinkedHashSet<>();
+    }
+    
+    public User(String ip, String username, LinkedHashSet<Chat> chats) {  // existing user (info from server ???)
+        this.ip = ip;
+        this.username = username;
+        this.chats = chats;
+    }
+    
+    // region Getters/Setters
+    public String getIp() {
+        return ip;
+    }
+    
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public LinkedHashSet<Chat> getChats() {
+        return chats;
+    }
+    // endregion
+    
+    // TODO fill in any more user data
+}
