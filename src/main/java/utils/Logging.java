529b24623e856b55af34d98f4ab958a12dd1a579
calciumking <lkingerslev@gmail.com>
2026-02-07 18:00:36 -0500

    Added Javadocs to Logging.java.

--- src/main/java/utils/Logging.java
+++ src/main/java/utils/Logging.java
@@ -3,6 +3,17 @@ package utils;
 import java.io.IOException;
 import java.util.logging.*;
 
+/**
+ * Logger utility for Hadrian application.
+ * <p>
+ * Initializes static logger for logging messages to
+ * a file with different severity levels.
+ * <p>
+ * Logger can be used by referencing {@code Logging.log()}, no initialization required.
+ *
+ * @see Logger
+ * @see Logging#log(String, Level)
+ */
 public class Logging {
     private static final Logger logger = Logger.getLogger(Logging.class.getName());
     
@@ -24,6 +35,14 @@ public class Logging {
         }
     }
     
+    /**
+     * Write logs to 'app.log' with timestamps, severity level, and message.
+     *
+     * @param message message to be displayed in log
+     * @param level severity of message as java utility logging object
+     * @see Logger
+     * @see Level
+     */
     public static void log(String message, Level level) {
         logger.log(level, message);
     }
