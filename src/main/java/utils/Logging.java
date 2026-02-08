e0be88973e4eab654f07a7b27841d02d9d9040ca
calciumking <lkingerslev@gmail.com>
2026-02-08 16:22:07 -0500

    Updated Logging.java
    - Made it final, indicating that nothing should inherit from it, but also that it doesn't need to be modified further.
    
    - Added overload Logging.log() method to display errors as well.
    
    - Updated Javadocs

--- src/main/java/utils/Logging.java
+++ src/main/java/utils/Logging.java
@@ -12,25 +12,27 @@ import java.util.logging.*;
  * Logger can be used by referencing {@code Logging.log()}, no initialization required.
  *
  * @see Logger
- * @see Logging#log(String, Level)
+ * @see #log(String, Level) Standard Logging Method
+ * @see #log(String, Level, Exception) Exception Logging Method
  */
-public class Logging {
+public final class Logging {
     private static final Logger logger = Logger.getLogger(Logging.class.getName());
     
     static {
         try {
+            // disable logs from appearing in terminal
             logger.setUseParentHandlers(false);
-            
             for (Handler h : logger.getHandlers())
                 logger.removeHandler(h);
             
+            // output to a .log file
             FileHandler handler = new FileHandler("app.log", true);
             handler.setFormatter(new SimpleFormatter());
-            
             logger.addHandler(handler);
-            logger.setLevel(Level.ALL);
+            
+            logger.setLevel(Level.ALL);  // display all levels of severity, not just error or warning
         } catch (IOException e) {
-            logger.log(Level.SEVERE, "Error setting up file logging", e);
+            log("Error setting up file logging", Level.SEVERE, e);
             throw new RuntimeException(e);
         }
     }
@@ -42,8 +44,25 @@ public class Logging {
      * @param level severity of message as java utility logging object
      * @see Logger
      * @see Level
+     * @see #log(String, Level, Exception) Exception Logging Method
      */
     public static void log(String message, Level level) {
         logger.log(level, message);
+    }
+    
+    /**
+     * Overload logging method to also display an exception
+     * <p>
+     * Write error log to 'app.log' with timestamp, severity level, message, and thrown error.
+     *
+     * @param message message to be displayed in log
+     * @param level severity of message as java utility logging object
+     * @param error error thrown with log
+     * @see Logger
+     * @see Level
+     * @see #log(String, Level) Standard Logging Method
+     */
+    public static void log(String message, Level level, Exception error) {
+        logger.log(level, message, error);
     }
 }
