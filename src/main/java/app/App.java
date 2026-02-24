62495cc2716fece6402624ea266bfb5eea060f57
calciumking <lkingerslev@gmail.com>
2026-02-23 22:39:21 -0500

    Adjusted App.java to run a Java Swing application and contain frontend-related utilities.
    - Frontend is not finished, error free but buggy
    
    - Renamed MessageController.java to ChatsController.java
    
    - Each controller has a .form file representing the page
    
    - AbstractController has been slightly adjusted for Swing classes

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,67 +1,74 @@
 package app;
 
-import controllers.AbstractController;
-import controllers.HelpController;
-import controllers.HomeController;
-import controllers.MessageController;
-import jexer.TApplication;
-import jexer.TWindow;
+import controllers.*;
 import models.Screens;
 import models.User;
 import utils.Logging;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.FileNotFoundException;
-import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 
-public class App extends TApplication {
-    private static TWindow root;
+public final class App {
     private static User user;
+    private static JFrame frame;
     
-    public App() throws UnsupportedEncodingException {
-        super(BackendType.XTERM);
-        
-        root = addWindow(
-                "Hadrian",
-                0, 0,
-                getScreen().getWidth(), getScreen().getHeight(),
-                TWindow.NOCLOSEBOX | TWindow.NOZOOMBOX | TWindow.OVERRIDEMENU
-        );
-        
-        new HomeController(root);
+    public static void main(String[] args) throws FileNotFoundException {
+        Config conf = new Config();
+        conf.readConfig();
         
         try {
-            user = new User(InetAddress.getLocalHost().getHostAddress(), "someUsername");
+            InetAddress host = InetAddress.getLocalHost();
+            user = new User(host.getHostAddress(), host.getHostName());
         } catch (UnknownHostException e) {
             Logging.log("Failed To Get User IP Address", Level.SEVERE, e);
             throw new RuntimeException(e);
         }
+        
+        frame = initJFrame();
     }
     
-    public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException {
-        Config conf = new Config();
-        conf.readConfig();
-        new App().run();
+    private static JFrame initJFrame() {
+        JFrame appFrame = new JFrame("Hadrian");
+        new HomeController(appFrame).init();
+        
+        appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        Rectangle bounds = ge.getMaximumWindowBounds();
+        double width = bounds.getWidth(), height = bounds.getHeight();
+        
+        appFrame.setPreferredSize(new Dimension((int) width, (int) height));
+        
+        appFrame.pack();
+        appFrame.setLocationRelativeTo(null);  // Set the frame location to the center of the screen
+        appFrame.setVisible(true);
+        
+        return appFrame;
     }
-    
-    public static void changeScreen(AbstractController current, Screens toScreen) {
-        if (current.screen == toScreen) {
-            String message = "Already on page: " + current.screen.name();
-            Logging.log(message, Level.INFO);
-            current.displayNotif(message);
-            return;
-        }
-        
-        current.hide();
-        
-        AbstractController controller = switch (toScreen) {
-            case HOME -> new HomeController(root);
-            case MESSAGES -> new MessageController(root);
-            case HELP -> new HelpController(root);
-            default -> throw new IllegalArgumentException("Unknown screen type:" + toScreen);
+
+    public static void changeScreen(Screens screen) {
+        AbstractController controller = switch (screen) {
+            case HOME -> new HomeController(frame);
+            case CHATS -> new ChatsController(frame);
+            case HELP -> new HelpController(frame);
+            default -> throw new IllegalArgumentException("Unknown screen type:" + screen);
         };
+        
+        controller.init();
+        
+        if (controller instanceof Loadable l)
+            l.load(user);
+        
+        draw();
+    }
+    
+    public static void draw() {
+        frame.revalidate();
+        frame.repaint();
     }
     
     public static User getUser() {
