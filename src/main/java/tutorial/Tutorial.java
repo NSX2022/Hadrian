6cdb3eeb4c9bdccc9d03568cfa645a8b6c9aec0d
calciumking <lkingerslev@gmail.com>
2026-02-25 21:11:50 -0500

    Slightly simplified Tutorial.java, changed tutorial messages slightly

--- src/main/java/tutorial/Tutorial.java
+++ src/main/java/tutorial/Tutorial.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public final class Tutorial extends Chat {
-    private final User user, tutorialBot;
+    private final User tutorialBot;
     
     public Tutorial(User user, User tutorialBot) {
         super(
@@ -16,15 +16,14 @@ public final class Tutorial extends Chat {
                 "This is the tutorial chat.",
                 user
         );
-        this.user = user;
         this.tutorialBot = tutorialBot;
         
         fillChat();
     }
     
     private void fillChat() {
-        addMessage("This is a practice chat — nothing you send here leaves your computer.", tutorialBot);
-        addMessage("You can type anything and press Enter to send it.", tutorialBot);
-        addMessage("Try sending me a text.", tutorialBot);
+        addMessage("Nothing you send here leaves your computer.", tutorialBot);
+        addMessage("You can type anything and press [Enter] to send it.", tutorialBot);
+        addMessage("Try sending me a message.", tutorialBot);
     }
 }
