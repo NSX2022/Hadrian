9432beead1d98adc315ed0ff103a6f9fe48ac81b
calciumking <lkingerslev@gmail.com>
2026-03-01 13:26:59 -0500

    Added new chat verification before chat creation
    - If either user field or message area are empty after removing whitespace, chat will not be created
    
    - User field is validated with regex expression
    
    - Messages are logged to file, but no visible notification has been established, will implement with future notification implementation

--- src/main/java/controllers/ChatsController.java
+++ src/main/java/controllers/ChatsController.java
@@ -5,10 +5,12 @@ import models.Chat;
 import models.ChatPanel;
 import models.Screens;
 import models.User;
+import utils.Logging;
 
 import javax.swing.*;
 import java.util.HashSet;
 import java.util.List;
+import java.util.logging.Level;
 
 public class ChatsController extends AbstractController implements Loadable {
     private final JFrame appFrame;
@@ -30,7 +32,7 @@ public class ChatsController extends AbstractController implements Loadable {
         
         bindKey("ESCAPE", "back", () -> App.changeScreen(Screens.HOME));
     }
-
+    
     @Override
     public void load(User user) {
         this.user = user;
@@ -48,6 +50,9 @@ public class ChatsController extends AbstractController implements Loadable {
     }
     
     private void createChat() {
+        if (invalidFields())
+            return;
+        
         HashSet<String> users = new HashSet<>(List.of(usersField.getText().split(",")));
         Chat chat = new Chat(users, messageArea.getText(), user);
         user.addChat(chat);
@@ -57,8 +62,31 @@ public class ChatsController extends AbstractController implements Loadable {
         controller.load(user);
         
         App.draw();
+        
+        Logging.log("Created New Chat With Members: " + usersField.getText(), Level.INFO);
     }
     
+    private boolean invalidFields() {
+        if (usersField.getText().isBlank() || messageArea.getText().isBlank()) {
+            // TODO blank field notification
+            Logging.log("One Or More Fields Are Blank", Level.WARNING);
+            return true;
+        }
+        
+        String regex = "^((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\.){3}" +
+                       "(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)" +
+                       "(\\s*,\\s*((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\.){3}" +
+                       "(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d))*$";
+        
+        if (!usersField.getText().matches(regex)) {
+            // TODO invalid IP listing
+            Logging.log("Invalid IP Listing", Level.WARNING);
+            return true;
+        }
+        
+        return false;
+    }
+
 //    @Override
 //    protected void show() {
 //        for (int i = 0; i < chats.size(); i++) {
